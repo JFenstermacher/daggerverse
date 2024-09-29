4aa15db73af2299d4a821429dac894a3b387b40b
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2024-09-29 15:23:27 -0500

    wip: working through some buf lint

---
+++ buf/main.go
@@ -0,0 +1,95 @@
+// A generated module for Buf functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"dagger/buf/internal/dagger"
+	"fmt"
+	"strings"
+)
+
+const (
+	WorkDir = "/workspace"
+)
+
+// Buf project
+type Buf struct {
+	// Go packages to install
+	// Each will be run with 'go install <package>'
+	// The following packages will be installed by default:
+	//   * github.com/bufbuild/buf/cmd/buf@latest
+	//   * google.golang.org/protobuf/cmd/protoc-gen-go@latest
+	//   * connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
+	Packages []string
+
+	// Project source directory
+	Source *dagger.Directory
+}
+
+func New(
+	// Project source directory
+	source *dagger.Directory,
+
+	// Packages to install
+	// +optional
+	// +default=[]
+	packages []string,
+) *Buf {
+	defPackages := map[string]string{
+		"github.com/bufbuild/buf/cmd/buf":                  "latest",
+		"google.golang.org/protobuf/cmd/protoc-gen-go":     "latest",
+		"connectrpc.com/connect/cmd/protoc-gen-connect-go": "latest",
+	}
+
+	for _, p := range packages {
+		parts := strings.Split(p, "@")
+		name := parts[0]
+
+		_, ok := defPackages[name]
+
+		if ok {
+			delete(defPackages, name)
+		}
+	}
+
+	for k, v := range defPackages {
+		packages = append(packages, fmt.Sprintf("%s@%s", k, v))
+	}
+
+	return &Buf{
+		Packages: packages,
+		Source:   source,
+	}
+}
+
+func (b *Buf) Container() *dagger.Container {
+	ctr := dag.
+		Container().
+		From("bufbuild/buf@latest").
+		WithWorkdir(WorkDir).
+		WithMountedDirectory(WorkDir, b.Source)
+
+	for _, p := range b.Packages {
+		ctr.WithExec([]string{"go", "install", p})
+	}
+
+	return ctr
+}
+
+func (b *Buf) Lint() *dagger.Container {
+	return b.
+		Container().
+		WithExec([]string{"buf", "lint"})
+}
